71f0697cc3d23a615f26b6c20a84c9facc846a68
Aman2745 <amandange779@gmail.com>
2023-06-29 11:43:04 +0530

    number app

---
+++ src/main/java/com/bootcoding/oops/service/number/NumberApp.java
@@ -0,0 +1,7 @@
+package com.bootcoding.oops.service.number;
+
+public class NumberApp {
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
---
+++ src/main/java/com/bootcoding/oops/service/number/model/Number.java
@@ -0,0 +1,8 @@
+package com.bootcoding.oops.service.number.model;
+
+public class Number {
+    private int num;
+    private String evenOdd;
+    private String primeNumber;
+    private String palindrom;
+}
